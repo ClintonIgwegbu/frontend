665ec833f2db840c99e678b147e927b77d894afd
Clinton Igwegbu <clinton.igwegbu@gmail.com>
2023-02-26 01:08:18 -0800

    Revert workspace navbar

--- frontend/modules/projects/components/WorkspaceNavbar.tsx
+++ frontend/modules/projects/components/WorkspaceNavbar.tsx
@@ -1,89 +1,13 @@
-import React, { useState, FunctionComponent } from 'react';
-import styles from '@styles/components/NavBar.module.scss';
-import Link from 'next/link';
-import { ButtonStyle, ButtonSize } from '../types/buttonTypes';
-import ButtonLink from './ButtonLink';
-import NavbarMenuItem from '../types/NavbarMenuItem';
+import React, { FunctionComponent } from 'react';
+import Navbar from '@modules/shared/components/Navbar';
 
-type NavbarProps = {
-  menuItems: NavbarMenuItem[];
-  showLoginButtons: boolean;
-  heightInPixels?: number;
+const WorkspaceNavbar: FunctionComponent = () => {
+  const menuItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Scripts', href: '/projects/1/workspace/scripts' },
+    { label: 'Editor', href: '/projects/1/workspace/editor' }
+  ];
+  return <Navbar menuItems={menuItems} showLoginButtons={false} heightInPixels={45} />;
 };
 
-const Navbar: FunctionComponent<NavbarProps> = ({
-  menuItems,
-  showLoginButtons,
-  heightInPixels
-}) => {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
-  const handleClick = () => setClick(!click);
-
-  // const showButton = () => {
-  //   if (window.innerWidth <= 960) {
-  //     setButton(false);
-  //   } else {
-  //     setButton(true);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   showButton();
-  // }, []);
-
-  // window.addEventListener('resize', showButton);
-
-  const navMenuItems = menuItems.map(menuItem => {
-    return (
-      <li key={menuItem.label} className={styles.navItem}>
-        <Link href={menuItem.href} className={styles.navLinks}>
-          {menuItem.label}
-        </Link>
-      </li>
-    );
-  });
-
-  const navMenu = (
-    <ul className={click ? `${styles.navMenu} ${styles.active}` : styles.navMenu}>
-      {navMenuItems}
-    </ul>
-  );
-
-  const loginButtons = (
-    <>
-      <Link href='/log-in' className={styles.navLinks}>
-        Log In
-      </Link>
-      {button && (
-        <ButtonLink
-          label='Sign Up'
-          href='/sign-up'
-          buttonStyle={ButtonStyle.Primary}
-          buttonSize={ButtonSize.Medium}
-        />
-      )}
-    </>
-  );
-  return (
-    <>
-      <nav
-        className={`${styles.navbar} ${styles.active}`}
-        style={{ height: `${heightInPixels}px` }}>
-        <div className={styles.navbarContainer}>
-          <Link href='/' className={styles.navbarLogo}>
-            {/* <div className={styles.logoText}>colLab</div> */}
-          </Link>
-          <div className={styles.menuIcon} onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          {navMenu}
-          {showLoginButtons && loginButtons}
-        </div>
-      </nav>
-    </>
-  );
-};
-
-export default Navbar;
+export default WorkspaceNavbar;
